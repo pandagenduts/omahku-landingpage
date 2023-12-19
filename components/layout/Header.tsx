6338e3c252649e9aca1ccbb785729502045d0db2
Donny <donny.rendi@yahoo.co.id>
2023-12-19 21:35:56 +0700

    build: responsive mobile menu

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,34 +1,81 @@
 'use client'
 
 import Link from 'next/link'
+import { Button } from '../ui/button'
+import { Menu as MenuIcon } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import { cn } from '@/lib/utils'
+
+const menus = [
+  { title: 'Buy House', href: '/' },
+  { title: 'Rent Apartment', href: '/' },
+  { title: 'Sell Properties', href: '/' },
+  { title: 'News & Article', href: '/' },
+  { title: 'About Us', href: '/' },
+]
 
 export default function Header() {
   return (
-    <header className='flex justify-between container'>
+    <header className='container flex items-center justify-between py-6'>
       <p>Logo</p>
-      <Menu />
-      <div>
-        <button>Log in</button>
-        <button>Sign up</button>
+      <DesktopMenu />
+      <MobileMenu />
+      <div className='hidden gap-4 lg:flex'>
+        <Button variant='ghost'>Log in</Button>
+        <Button>Sign up</Button>
       </div>
+      {/* <AuthMenu /> */}
     </header>
   )
 }
 
-function Menu() {
-  const menus = [
-    { title: 'Buy House', href: '/' },
-    { title: 'Rent Apartment', href: '/' },
-    { title: 'Sell Properties', href: '/' },
-    { title: 'News & Article', href: '/' },
-    { title: 'About Us', href: '/' },
-  ]
-
+function DesktopMenu() {
   return (
-    <nav>
+    <nav className='hidden lg:block'>
       <menu className='flex gap-10'>
-        {menus?.map((menu) => <Link href={menu.href}>{menu.title}</Link>)}
+        {menus?.map((menu) => <Link href={menu.href} key={menu.title}>{menu.title}</Link>)}
       </menu>
     </nav>
+  )
+}
+
+function MobileMenu() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <MenuIcon className='h-6 w-6 lg:hidden' />
+      </SheetTrigger>
+      <SheetContent className='flex flex-col'>
+        <SheetHeader className='mb-8'>
+          <SheetTitle>Omahku</SheetTitle>
+        </SheetHeader>
+        <div className='flex flex-1 flex-col'>
+          <menu className='flex flex-1 flex-col gap-4'>
+            {menus?.map((menu) => <Link href={menu.href} key={menu.title}>{menu.title}</Link>)}
+          </menu>
+          <div className='flex flex-col gap-4'>
+            <Button variant='ghost'>Log in</Button>
+            <Button>Sign up</Button>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+function AuthMenu(props: { parentClassName?: string }) {
+  const { parentClassName } = props
+
+  return (
+    <div className={cn('flex gap-4', parentClassName)}>
+      <Button variant='ghost'>Log in</Button>
+      <Button>Sign up</Button>
+    </div>
   )
 }
