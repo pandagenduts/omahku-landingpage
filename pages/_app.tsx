d14059ed5ecbb40a2fd55bfe469ba8efeb8243eb
Donny <donny.rendi@yahoo.co.id>
2023-12-19 19:25:39 +0700

    build: initial commit & basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter, Plus_Jakarta_Sans } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  variable: '--font-plus-jakarta-sans',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main className={`${plusJakartaSans.className} ${inter.variable}`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
