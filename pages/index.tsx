8d6081f95782f8461a1dfb3dc98ecd2cd923ab1a
Donny <donny.rendi@yahoo.co.id>
2023-12-20 20:24:56 +0700

    refactor: value section folder & file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import HeroBottom from "@/components/home/hero-section/HeroBottom";
 import HeroTop from "@/components/home/hero-section/HeroTop";
 import ClientSection from "@/components/home/ClientSection";
-import ValueSection from "@/components/home/value-section/ValueSection";
+import ValueSection from "@/components/home/ValueSection";
 import FeaturedSection from "@/components/home/FeaturedSection";
 import TestiSection from "@/components/home/TestiSection";
 
