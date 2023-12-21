90a5812fa14c25d8e46772b29fdc0149d2f337f3
Donny <donny.rendi@yahoo.co.id>
2023-12-21 15:18:23 +0700

    style: testi section responsive

--- components/home/TestiSection.tsx
+++ components/home/TestiSection.tsx
@@ -7,38 +7,43 @@ import { TestiCard } from '@/lib/types'
 
 export default function TestiSection() {
   return (
-    <section className='container mb-[98px] md:mb-[73px]' id='testimonials'>
-      <div className='mb-[58px] flex flex-col items-end md:mb-[72px] md:flex-row md:gap-16'>
-        <div className='mb-6 flex w-full flex-col items-center md:mb-0 md:items-start'>
-          <Pill>Testimonials</Pill>
-          <h2 className='text-center md:text-left'>
-            What Our Beloved Customers Say About Omahku
-          </h2>
+    <section
+      className='mb-[98px] overflow-hidden md:mb-[73px]'
+      id='testimonials'
+    >
+      <div className='container'>
+        <div className='mb-[58px] flex flex-col items-end md:mb-[72px] md:flex-row md:gap-16'>
+          <div className='mb-6 flex w-full flex-col items-center md:mb-0 md:items-start'>
+            <Pill>Testimonials</Pill>
+            <h2 className='text-center md:text-left'>
+              What Our Beloved Customers Say About Omahku
+            </h2>
+          </div>
+          <p className='w-full text-center text-secGray2 md:text-left'>
+            Whether you&rsquo;re buying, selling, or investing in real estate,
+            we have the expertise, resources, and dedication to help you achieve
+            your goals. Contact us today to learn more about how we can help you
+            with your real estate needs.
+          </p>
         </div>
-        <p className='w-full text-center text-secGray2 md:text-left'>
-          Whether you&rsquo;re buying, selling, or investing in real estate, we
-          have the expertise, resources, and dedication to help you achieve your
-          goals. Contact us today to learn more about how we can help you with
-          your real estate needs.
-        </p>
-      </div>
 
-      <div id='swiper-container'>
-        <Swiper
-          spaceBetween={36}
-          breakpoints={{
-            768: {
-              slidesPerView: 'auto',
-            },
-          }}
-          slidesPerView={1}
-        >
-          {homeTestimonials?.map((testi, index) => (
-            <SwiperSlide key={index}>
-              <TestiCard key={testi.name} data={testi} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <div id='swiper-container'>
+          <Swiper
+            spaceBetween={36}
+            breakpoints={{
+              768: {
+                slidesPerView: 'auto',
+              },
+            }}
+            slidesPerView={1}
+          >
+            {homeTestimonials?.map((testi, index) => (
+              <SwiperSlide key={index}>
+                <TestiCard key={testi.name} data={testi} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </section>
   )
@@ -48,8 +53,8 @@ function TestiCard(props: TestiCard) {
   const { name, picture, role, testimonial } = props.data
 
   return (
-    <div className='flex h-full min-h-[268px] flex-col justify-between rounded-[20px] bg-secGray5 px-6 py-8 md:min-h-[304px] md:w-[544px] md:py-9 md:px-12'>
-      <p className='font-inter text-base md:text-xl font-medium text-[#3e3e3e] mb-[26px] md:mb-12'>
+    <div className='flex h-full min-h-[268px] flex-col justify-between rounded-[20px] bg-secGray5 px-6 py-8 md:min-h-[304px] md:w-[544px] md:px-12 md:py-9'>
+      <p className='mb-[26px] font-inter text-base font-medium text-[#3e3e3e] md:mb-12 md:text-xl'>
         {testimonial}
       </p>
       <div className='flex gap-4'>
