e96f64a4712b6d93ccf893eda79028bfbc8a196f
Donny <donny.rendi@yahoo.co.id>
2023-12-21 17:34:08 +0700

    style: footer top h2 responsive

--- components/layout/footer/FooterTop.tsx
+++ components/layout/footer/FooterTop.tsx
@@ -4,8 +4,9 @@ export default function FooterTop() {
   return (
     <section className='mx-auto mb-[60px] w-full max-w-[1255px] bg-[url("/footer-bg.png")] bg-cover bg-center bg-no-repeat px-5 pb-[252px] pt-[74px] md:mb-20 md:px-9 md:pb-9 md:pt-12 lg:rounded-2xl'>
       <div className='mx-auto w-full max-w-[618px]'>
-        <h2 className='mb-4 text-center'>
-          Don’t Need to Wait Start Invest Your Future From Now.
+        <h2 className='mx-auto mb-4 max-w-[300px] text-center sm:max-w-none'>
+          Don’t Need to Wait
+          <br /> Start Invest Your Future From Now.
         </h2>
         <p className='mb-12 text-center text-secGray2'>
           Contact us to schedule a consultation and learn how we can help you
